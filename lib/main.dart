aadbfd6365798b265996f11addc95c29e925a075
snova <awaawapurin@gmail.com>
2021-05-23 03:38:10 +0900

    ✨ feat: Add gradient app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UOA EV Calculator',
+      title: 'ユニエア 期待値計算機',
       theme: ThemeData.light(),
-      home: Container(),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: NewGradientAppBar(
+        title: Text('期待値計算機'),
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(241, 157, 181, 1.0),
+            Color.fromRGBO(124, 199, 232, 1.0),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(),
+      ),
     );
   }
 }
