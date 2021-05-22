519282c7ce2d9a8e246d1125a2c620aca3472886
snova <awaawapurin@gmail.com>
2021-05-23 01:27:53 +0900

    🎉 init: Initial commit

---
+++ android/app/src/main/kotlin/com/example/uoa_ev_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uoa_ev_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UOA EV Calculator',
+      theme: ThemeData.light(),
+      home: Container(),
+    );
+  }
+}
