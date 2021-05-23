b7df2ca8a37dcd99d1780191f561b9b8a67b3e27
snova <awaawapurin@gmail.com>
2021-05-24 03:17:03 +0900

    ☪️ refactor: Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
+
+import './view/main_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,26 +13,6 @@ class MyApp extends StatelessWidget {
       title: 'ユニエア 期待値計算機',
       theme: ThemeData.light(),
       home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: NewGradientAppBar(
-        title: Text('期待値計算機'),
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(241, 157, 181, 1.0),
-            Color.fromRGBO(124, 199, 232, 1.0),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(),
-      ),
     );
   }
 }
