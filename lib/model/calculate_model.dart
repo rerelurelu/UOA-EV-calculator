623ff225c1d35db7961bdd1de7bdc4e59aec297b
snova <awaawapurin@gmail.com>
2021-05-25 23:21:47 +0900

    ☪️ refactor: Add explanation of the arguments.

--- lib/model/calculate_model.dart
+++ lib/model/calculate_model.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CalculateModel extends ChangeNotifier {
-  double expectedValue = 0.0;
+  double expectedValue = 0.00;
 
   void calculateExpectedValue(int incr, int prob, int interval, int time) {
+    /**
+     * incr -> スコア○％アップ
+     * prob -> ○％の確率で
+     * interval -> ○秒おきに
+     * time -> ○秒間
+     */
     expectedValue = (incr / 100) *
         (prob * time) /
         ((interval + time) * prob + interval * (1 - prob));
