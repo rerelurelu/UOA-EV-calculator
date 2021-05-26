6be2b02715b94ba27d666ab0288debc0fa62e527
snova <awaawapurin@gmail.com>
2021-05-26 15:47:49 +0900

    ✨ feat: Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './view/main_page.dart';
+import './model/calculate_model.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      ChangeNotifierProvider(
+        create: (_) => CalculateModel(),
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
