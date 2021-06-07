1f3a7df56fdbe94e166b724a9517e90d48f431f6
snova <awaawapurin@gmail.com>
2021-06-07 15:39:13 +0900

    ☪️ refactor: Separate theme data into new file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import './view/main_page.dart';
+import './theme/main_theme.dart';
 
 void main() => runApp(
       ProviderScope(
@@ -15,24 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ユニエア 期待値計算機',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xff7cc7e8),
-        inputDecorationTheme: InputDecorationTheme(
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: Color(0xff67b7b2),
-            ),
-          ),
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.white,
-            ),
-          ),
-          hintStyle: TextStyle(
-            color: Color(0xff577979),
-          ),
-        ),
-      ),
+      theme: hinataTheme,
       home: MainPage(),
     );
   }
