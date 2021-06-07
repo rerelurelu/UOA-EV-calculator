8a0162f0562f95b32748aafb3df0af511c1243b9
snova <awaawapurin@gmail.com>
2021-06-07 23:14:48 +0900

    ✨ feat: Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import './view/main_page.dart';
-import './theme/main_theme.dart';
+import 'theme/theme_data.dart';
 
-void main() => runApp(
-      ProviderScope(
-        child: MyApp(),
-      ),
-    );
+void main() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+
+  return runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ユニエア 期待値計算機',
-      theme: hinataTheme,
-      home: MainPage(),
+    return Consumer(
+      builder: (context, watch, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'ユニエア 期待値計算機',
+          theme: hinataTheme,
+          home: MainPage(),
+        );
+      },
     );
   }
 }
