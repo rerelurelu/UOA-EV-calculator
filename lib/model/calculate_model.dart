3dd802aeecfb61770bc5e126d3736a01e570eb29
zoniha <awaawapurin@gmail.com>
2021-07-06 16:42:04 +0900

    🥰 perf: Add default Exvalue

--- lib/model/calculate_model.dart
+++ lib/model/calculate_model.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CalculateModel extends ChangeNotifier {
-  String expectedValue = '0.00';
+  static String defaultValue = '0.00';
+  String expectedValue = defaultValue;
   String get getExvalue => expectedValue;
 
   void calculate(int interval, int prob, int time, int incr) {
-    /**
+    /*
      * incr -> スコア○％アップ
      * prob -> ○％の確率で
      * interval -> ○秒おきに
      * time -> ○秒間
-     **/
+     */
     double result =
         incr * (prob / 100 * time) / ((interval + time) * prob / 100 + interval * (1 - prob / 100));
 
@@ -20,7 +21,7 @@ class CalculateModel extends ChangeNotifier {
   }
 
   void resetValue() {
-    expectedValue = '0.0';
+    expectedValue = defaultValue;
     notifyListeners();
   }
 }
